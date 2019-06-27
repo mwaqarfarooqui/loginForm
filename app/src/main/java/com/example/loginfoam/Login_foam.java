cef234ad38f389f751a575dc2305a0631ff1e015
mwaqarfarooqui <waqarfarooqui.12@gmail.com>
2019-06-27 16:05:01 +0500

    initial commit

---
+++ app/src/main/java/com/example/loginfoam/Login_foam.java
@@ -0,0 +1,14 @@
+package com.example.loginfoam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_foam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_foam);
+    }
+}
